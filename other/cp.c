5c5e11cd7e9b64bc66d6ac49a860df5af5a46f5d
bin7sk <bin7sk@gmail.com>
2021-12-14 19:31:16 +0300

    self-made cp

---
+++ other/cp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+ self-made cp
+   */
+int main(int argc, char* argv[]){
+	if(argc != 3){
+		printf("Program must have 3 arguments!\n");
+		return 1;
+	}
+	FILE *src = fopen(argv[1], "r");
+	FILE *dest = fopen(argv[2], "w");
+	int c;
+	while((c=fgetc(src))!= EOF) fputc(c, dest);
+	return 0;
+
+}
